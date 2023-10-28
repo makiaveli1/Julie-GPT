0d4c77e91f808ee152ec7accd1c306104f2c7e0a
makiaveli1 <itsmakiaveli@gmail.com>
2023-10-28 14:01:01 +0100

    Add basic chatbot interface setup

--- Julie_bot/Julie_bot/settings.py
+++ Julie_bot/Julie_bot/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'Julie_bot.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':  [TEMPLATES_DIR],
+        'DIRS':  [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
