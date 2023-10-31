acf9e4a7197138b3b9f5b1ebb498ae79c1e4bada
makiaveli1 <itsmakiaveli@gmail.com>
2023-10-31 17:47:22 +0000

    setup basic chatbot with openai api and views for chatbot

--- Julie_bot/chatbot/views.py
+++ Julie_bot/chatbot/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import openai
 
 # Create your views here.
+def openai_request(message):
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": message}
+            ]
+        )
+        
+        answer = response['choices'][0]['message']['content']
+        return answer.strip()
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
 def chatbot(request):
 
     if request.method == 'POST':
         message = request.POST.get('message')
-        response = 'hello'
+        response = openai_request(message)
         return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
