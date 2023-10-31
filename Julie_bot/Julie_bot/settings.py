61e53bfb3790e07dfb64f281f6e70ab7153d628e
makiaveli1 <itsmakiaveli@gmail.com>
2023-10-31 13:21:30 +0000

    add ability to display the chat to show the conversations between user and chatbot(UI/UX design still subject to changes)

--- Julie_bot/Julie_bot/settings.py
+++ Julie_bot/Julie_bot/settings.py
@@ -26,7 +26,6 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
-print(os.getenv('SECRET_KEY'))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
--- Julie_bot/chatbot/views.py
+++ Julie_bot/chatbot/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 def chatbot(request):
+
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = 'hello'
+        return JsonResponse({'message': message, 'response': response})
     return render(request, 'chatbot.html')
