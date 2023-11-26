e2564178bd1346dc1c316bd52a80c733cef81de9
makiaveli1 <itsmakiaveli@gmail.com>
2023-11-26 08:52:36 +0000

    Update the stylings for the chat page and style the login page as well

--- Julie_bot/chatbot/models.py
+++ Julie_bot/chatbot/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class CustomUser(AbstractUser):
     phone = models.CharField(max_length=15, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    profile_picture = models.ImageField(
+        upload_to='profile_pics/', null=True, blank=True)
     profile_picture_url = models.URLField(blank=True, null=True)
+
 
 class Chat(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
@@ -14,6 +17,10 @@ class Chat(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     messages = models.JSONField(default=list)
+
+    def message_exists(self, client_message_id):
+        return any(msg.get('client_message_id') ==
+                   client_message_id for msg in self.messages)
 
     def __str__(self):
         return f'{self.user.username} - Last updated: {self.updated_at}'
